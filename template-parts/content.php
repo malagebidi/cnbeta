cc2842f53a7dbe31669a7b8bf7e80e188e18e72c
Deng Yang <726456+malagebidi@users.noreply.github.com>
2024-03-16 17:18:41 +0800

    Update content.php

--- template-parts/content.php
+++ template-parts/content.php
@@ -13,7 +13,7 @@
 			<div class="entry-header col">
 
 				<h2 class="entry-title fs-6 lh-sm fw-bold">
-					<a class="text-decoration-none link-dark stretched-link" href="<?php the_permalink(); ?>" rel="bookmark"><?php echo mb_strimwidth( get_the_title(), 0, 65, "...", "utf-8" ); ?></a>
+					<a class="text-decoration-none link-dark stretched-link" href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
 				</h2>
 
 				<div class="entry-date text-secondary small">
