bcb9e9377454ade803a8554e6fc2c15403bacec3
Deng Yang <malagebidi@gmail.com>
2024-06-29 20:52:14 +0800

    Add files via upload

--- template-parts/content-link.php
+++ template-parts/content-link.php
@@ -23,20 +23,18 @@
 
 			<div class="entry-header">
 
-				<h2 class="entry-title fs-6 lh-base fw-bold">
+				<h2 class="entry-title fs-6 lh-base fw-bold mb-1">
 					<a class="text-decoration-none link-dark stretched-link" target="_blank" href="<?php echo wp_strip_all_tags(get_the_excerpt()); ?>" rel="bookmark">↗<?php the_title(); ?></a>
 				</h2>
 
 				<div class="entry-meta text-secondary small">
-					<?php echo esc_html( human_time_diff( get_the_time('U'), current_time('timestamp') ) ) . ' ago · '; ?>
 					<?php
+						$date = get_the_time();
 						$category = get_the_category();
 						$parent = get_cat_name($category[0]->category_parent);
-							if (!empty($parent)) {
-								echo $parent . ' > ' . $category[0]->cat_name;
-							} else {
-								echo $category[0]->cat_name;
-							}
+						$category_output = !empty($parent) ? $parent . ' > ' . $category[0]->cat_name : $category[0]->cat_name;
+
+						echo $date . ' · ' . $category_output;
 					?>
 				</div>
 
--- template-parts/content-video.php
+++ template-parts/content-video.php
@@ -10,7 +10,18 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'position-relative border-bottom pb-3 mb-3' ); ?>>
 		
-			<div class="post-thumbnail mb-2">
+			<div class="position-relative post-thumbnail mb-2">
+				<button class="play-button">
+					<svg viewBox="0 0 60 60">
+						<g>
+							<path d="M45.563,29.174l-22-15c-0.307-0.208-0.703-0.231-1.031-0.058C22.205,14.289,22,14.629,22,15v30
+									c0,0.371,0.205,0.711,0.533,0.884C22.679,45.962,22.84,46,23,46c0.197,0,0.394-0.059,0.563-0.174l22-15
+									C45.836,30.64,46,30.331,46,30S45.836,29.36,45.563,29.174z M24,43.107V16.893L43.225,30L24,43.107z"/>
+							<path d="M30,0C13.458,0,0,13.458,0,30s13.458,30,30,30s30-13.458,30-30S46.542,0,30,0z M30,58C14.561,58,2,45.439,2,30
+									S14.561,2,30,2s28,12.561,28,28S45.439,58,30,58z"/>
+						</g>
+					</svg>
+				</button>
 				<?php the_post_thumbnail(
     					'full-thumbnail',
     					array(
@@ -23,20 +34,18 @@
 
 			<div class="entry-header">
 
-				<h2 class="entry-title fs-6 lh-base fw-bold">
+				<h2 class="entry-title fs-6 lh-base fw-bold mb-1">
 					<a class="text-decoration-none link-dark stretched-link" href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
 				</h2>
 
 				<div class="entry-meta text-secondary small">
-					<?php echo esc_html( human_time_diff( get_the_time('U'), current_time('timestamp') ) ) . ' ago · '; ?>
 					<?php
+						$date = get_the_time();
 						$category = get_the_category();
 						$parent = get_cat_name($category[0]->category_parent);
-							if (!empty($parent)) {
-								echo $parent . ' > ' . $category[0]->cat_name;
-							} else {
-								echo $category[0]->cat_name;
-							}
+						$category_output = !empty($parent) ? $parent . ' > ' . $category[0]->cat_name : $category[0]->cat_name;
+
+						echo $date . ' · ' . $category_output;
 					?>
 				</div>
 
