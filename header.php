5acff585e31f24f360d94fb213d7c14b5e6e4d7d
Deng Yang <malagebidi@gmail.com>
2024-07-04 07:58:31 +0800

    Add files via upload

--- header.php
+++ header.php
@@ -16,7 +16,6 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<meta name="referrer" content="never">
 
 	<?php wp_head(); ?>
 	<meta name="theme-color" content="#fff" media="(prefers-color-scheme: light)">
